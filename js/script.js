93d334e4f5b583886295d05eceb67d438cc59342
Alex275054 <55253968+Alex275054@users.noreply.github.com>
2022-07-12 17:46:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+// бургер
+$(document).ready(function () {
+   $('.header__burger').click(function (event) {
+      $('.header__burger,.header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+});
+
+// Popup
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+   for (let index = 0; index < popupLinks.length; index++) {
+      const popupLink = popupLinks[index];
+      popupLink.addEventListener("click", function (e) {
+         const popupName = popupLink.getAttribute('href').replace('#', '');
+         const curentPopup = document.getElementById(popupName);
+         popupOpen(curentPopup);
+         e.preventDefault();
+      });
+   }
+}
+
+const popupCloseIcon = document.querySelectorAl('.close-popup');
+if (popupCloseIcon.length > 0) {
+   for (let index = 0; index < popupCloseIcon.length; index++) {
+      const el = popupCloseIcon[index];
+      el.addEventListener('click', function (e) {
+         popupClose(el.close('.popup'));
+         e.preventDefault();
+      })
+   }
+}
+
+
+function popupOpen(curentPopup) {
+   if (curentPopup && unlock) {
+      const popupActive = document.querySelector('.popup.open');
+      if (popupActive) {
+         popupClose(popupActive, false);
+      } else {
+         bodyLock();
+      }
+      curentPopup.classList.add('open');
+      curentPopup.addEventListener("click", function (e) {
+         if (!e.target.closest('.popup__content')) {
+            popupClose(e.target.closest('.popup'));
+         }
+      });
+   }
+}
